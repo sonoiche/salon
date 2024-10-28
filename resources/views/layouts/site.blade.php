9a4e93657185817d83a8fbb69257f0d7fd064021
Jelson Lanto <jelsonlanto@Jelsons-iMac.local>
2024-10-28 23:04:16 +0800

    remove unnecessary button

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -86,14 +86,16 @@
                                 </div>
                                 @endif
                             </a>
-                           
+                            
+                            @if (auth()->check())
                             <!-- Nav Search -->
-                           <div class="nav-search py-15">
+                            <div class="nav-search py-15">
                                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="fas fa-sign-out-alt"></a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
                             </div>
+                            @endif
                         </div>
                     </div>
                 </div>
